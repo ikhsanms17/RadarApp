b2fd141bcd57982943866291b3db154a666b0b24
ikhsanms17 <maolanaikhsan31@gmail.com>
2023-08-02 17:06:55 +0700

    update

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -43,7 +43,7 @@ public class App extends Application {
     private TextSymbol numText, numText1, numText2, numText3, numText4,
             numText5, numText6, numText7, numText8, numText9, numText10, numText11;
     private SimpleMarkerSymbol symbol, circle2, circle3, circle4, circle5, circle6;
-    private SimpleLineSymbol stroke, lineSymbol;
+    private SimpleLineSymbol stroke, lineSymbol, lineRad;
     private VBox displayInfo;
     private AnimationTimer animationTimer;
     private Label label, labelLat, labelLong, labelDistance;
@@ -86,8 +86,8 @@ public class App extends Application {
         mapView.setMap(map);
         mapView.setViewpoint(new Viewpoint(cenX, cenY, scale));
         mapView.setOnScroll(null);
-        mapView.setOnMousePressed(null);
-        mapView.setOnMouseDragged(null);
+//        mapView.setOnMousePressed(null);
+//        mapView.setOnMouseDragged(null);
 
         // set title, scene and show for scene
         primaryStage.setTitle("Prototype Radar App");
@@ -156,7 +156,7 @@ public class App extends Application {
             @Override
             public void handle(long now) {
                 // update the rotation angle
-                rotationAngle += -1;
+                rotationAngle += -2;
                 if (rotationAngle >= 360) {
                     rotationAngle = 0;
                 }
@@ -168,6 +168,128 @@ public class App extends Application {
 
         // start the animation
         animationTimer.start();
+
+        lineRad = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.WHITE, 2);
+
+        for (int i = 0; i < 360; i+=2){
+            // create the start point of the scanning area line
+            double startX = centerPoint.getX() + 0.0536 * Math.cos(Math.toRadians(i));
+            double startY = centerPoint.getY() + 0.0536 * Math.sin(Math.toRadians(i));
+            Point startPoint = new Point(startX, startY, SpatialReferences.getWgs84());
+
+            // calculate the end point of the scanning area line based on the rotation angle
+            double endX = centerPoint.getX() + 0.0518 * Math.cos(Math.toRadians(i));
+            double endY = centerPoint.getY() + 0.0518 * Math.sin(Math.toRadians(i));
+            Point endPoint = new Point(endX, endY, SpatialReferences.getWgs84());
+
+            // create a polyline from the start and end points
+            PointCollection points = new PointCollection(SpatialReferences.getWgs84());
+            points.add(startPoint);
+            points.add(endPoint);
+            Polyline polyline = new Polyline(points);
+            Graphic lineGraph = new Graphic(polyline, lineRad);
+            graphicsOverlay.getGraphics().add(lineGraph);
+        }
+
+        for (int i = 0; i < 360; i+=10){
+            // create the start point of the scanning area line
+            double startX1 = centerPoint.getX() + 0.0553 * Math.cos(Math.toRadians(i));
+            double startY1 = centerPoint.getY() + 0.0553 * Math.sin(Math.toRadians(i));
+            Point startPoint1 = new Point(startX1, startY1, SpatialReferences.getWgs84());
+
+            // calculate the end point of the scanning area line based on the rotation angle
+            double endX1 = centerPoint.getX() + 0.051 * Math.cos(Math.toRadians(i));
+            double endY1 = centerPoint.getY() + 0.051 * Math.sin(Math.toRadians(i));
+            Point endPoint1 = new Point(endX1, endY1, SpatialReferences.getWgs84());
+
+            // create a polyline from the start and end points
+            PointCollection points1 = new PointCollection(SpatialReferences.getWgs84());
+            points1.add(startPoint1);
+            points1.add(endPoint1);
+            Polyline polyline1 = new Polyline(points1);
+            Graphic lineGraph1 = new Graphic(polyline1, lineRad);
+            graphicsOverlay.getGraphics().add(lineGraph1);
+        }
+
+        for (int i = 0; i < 360; i+=30){
+            // create the start point of the scanning area line
+            double startX2 = centerPoint.getX() + 0.0405 * Math.cos(Math.toRadians(i));
+            double startY2 = centerPoint.getY() + 0.0405 * Math.sin(Math.toRadians(i));
+            Point startPoint2 = new Point(startX2, startY2, SpatialReferences.getWgs84());
+
+            // calculate the end point of the scanning area line based on the rotation angle
+            double endX2 = centerPoint.getX() + 0.043 * Math.cos(Math.toRadians(i));
+            double endY2 = centerPoint.getY() + 0.043 * Math.sin(Math.toRadians(i));
+            Point endPoint2 = new Point(endX2, endY2, SpatialReferences.getWgs84());
+
+            // create a polyline from the start and end points
+            PointCollection points2 = new PointCollection(SpatialReferences.getWgs84());
+            points2.add(startPoint2);
+            points2.add(endPoint2);
+            Polyline polyline2 = new Polyline(points2);
+            Graphic lineGraph2 = new Graphic(polyline2, lineRad);
+            graphicsOverlay.getGraphics().add(lineGraph2);
+        }
+
+        for (int i = 0; i < 360; i+=30){
+            // create the start point of the scanning area line
+            double startX3 = centerPoint.getX() + 0.027 * Math.cos(Math.toRadians(i));
+            double startY3 = centerPoint.getY() + 0.027 * Math.sin(Math.toRadians(i));
+            Point startPoint3 = new Point(startX3, startY3, SpatialReferences.getWgs84());
+
+            // calculate the end point of the scanning area line based on the rotation angle
+            double endX3 = centerPoint.getX() + 0.0295 * Math.cos(Math.toRadians(i));
+            double endY3 = centerPoint.getY() + 0.0295 * Math.sin(Math.toRadians(i));
+            Point endPoint3 = new Point(endX3, endY3, SpatialReferences.getWgs84());
+
+            // create a polyline from the start and end points
+            PointCollection points3 = new PointCollection(SpatialReferences.getWgs84());
+            points3.add(startPoint3);
+            points3.add(endPoint3);
+            Polyline polyline3 = new Polyline(points3);
+            Graphic lineGraph3 = new Graphic(polyline3, lineRad);
+            graphicsOverlay.getGraphics().add(lineGraph3);
+        }
+
+        for (int i = 0; i < 360; i+=30){
+            // create the start point of the scanning area line
+            double startX4 = centerPoint.getX() + 0.0138 * Math.cos(Math.toRadians(i));
+            double startY4 = centerPoint.getY() + 0.0138 * Math.sin(Math.toRadians(i));
+            Point startPoint4 = new Point(startX4, startY4, SpatialReferences.getWgs84());
+
+            // calculate the end point of the scanning area line based on the rotation angle
+            double endX4 = centerPoint.getX() + 0.0163 * Math.cos(Math.toRadians(i));
+            double endY4 = centerPoint.getY() + 0.0163 * Math.sin(Math.toRadians(i));
+            Point endPoint4 = new Point(endX4, endY4, SpatialReferences.getWgs84());
+
+            // create a polyline from the start and end points
+            PointCollection points4 = new PointCollection(SpatialReferences.getWgs84());
+            points4.add(startPoint4);
+            points4.add(endPoint4);
+            Polyline polyline4 = new Polyline(points4);
+            Graphic lineGraph4 = new Graphic(polyline4, lineRad);
+            graphicsOverlay.getGraphics().add(lineGraph4);
+        }
+
+        for (int i = 0; i < 360; i+=90){
+            // create the start point of the scanning area line
+            double startX5 = centerPoint.getX() + 0.0000 * Math.cos(Math.toRadians(i));
+            double startY5 = centerPoint.getY() + 0.0000 * Math.sin(Math.toRadians(i));
+            Point startPoint5 = new Point(startX5, startY5, SpatialReferences.getWgs84());
+
+            // calculate the end point of the scanning area line based on the rotation angle
+            double endX5 = centerPoint.getX() + 0.0015 * Math.cos(Math.toRadians(i));
+            double endY5 = centerPoint.getY() + 0.0015 * Math.sin(Math.toRadians(i));
+            Point endPoint5 = new Point(endX5, endY5, SpatialReferences.getWgs84());
+
+            // create a polyline from the start and end points
+            PointCollection points5 = new PointCollection(SpatialReferences.getWgs84());
+            points5.add(startPoint5);
+            points5.add(endPoint5);
+            Polyline polyline5 = new Polyline(points5);
+            Graphic lineGraph5 = new Graphic(polyline5, lineRad);
+            graphicsOverlay.getGraphics().add(lineGraph5);
+        }
 
     }
 
@@ -206,6 +328,10 @@ public class App extends Application {
                         new AngularUnit(AngularUnitId.DEGREES),
                         GeodeticCurveType.GEODESIC).getDistance();
 
+//                double sudut_rad = Math.toRadians(sudut);
+//                double lat = centerPoint.getY() + (distance * Math.cos(sudut_rad));
+//                double lon = centerPoint.getX() + (distance * Math.sin(sudut_rad));
+
                 // display lat long
                 DecimalFormat df = new DecimalFormat("0.000000");
                 DecimalFormat df2 = new DecimalFormat("0.000");
@@ -239,7 +365,7 @@ public class App extends Application {
         circle5 = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.TRANSPARENT, 125);
         circle5.setOutline(stroke);
 
-        circle6 = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.WHITE, 5);
+        circle6 = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.WHITE, 2);
 
         // Create the graphic with the start point and symbol
         Graphic graphic2 = new Graphic(centerPoint, circle2);
@@ -292,62 +418,62 @@ public class App extends Application {
 
         numText = new TextSymbol(12, "0", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText.setOffsetY(265);
+        numText.setOffsetY(270);
         numText.setAngle(0);
 
         numText1 = new TextSymbol(12, "30", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText1.setOffsetY(265);
+        numText1.setOffsetY(270);
         numText1.setAngle(30);
 
         numText2 = new TextSymbol(12, "60", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText2.setOffsetY(265);
+        numText2.setOffsetY(270);
         numText2.setAngle(60);
 
         numText3 = new TextSymbol(12, "90", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText3.setOffsetY(265);
+        numText3.setOffsetY(270);
         numText3.setAngle(90);
 
         numText4 = new TextSymbol(12, "120", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText4.setOffsetY(265);
+        numText4.setOffsetY(270);
         numText4.setAngle(120);
 
         numText5 = new TextSymbol(12, "150", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText5.setOffsetY(265);
+        numText5.setOffsetY(270);
         numText5.setAngle(150);
 
         numText6 = new TextSymbol(12, "180", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText6.setOffsetY(265);
+        numText6.setOffsetY(270);
         numText6.setAngle(180);
 
         numText7 = new TextSymbol(12, "210", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText7.setOffsetY(265);
+        numText7.setOffsetY(270);
         numText7.setAngle(210);
 
         numText8 = new TextSymbol(12, "240", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText8.setOffsetY(265);
+        numText8.setOffsetY(270);
         numText8.setAngle(240);
 
         numText9 = new TextSymbol(12, "270", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText9.setOffsetY(265);
+        numText9.setOffsetY(267);
         numText9.setAngle(270);
 
         numText10 = new TextSymbol(12, "300", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText10.setOffsetY(265);
+        numText10.setOffsetY(270);
         numText10.setAngle(300);
 
         numText11 = new TextSymbol(12, "330", Color.WHITE,
                 TextSymbol.HorizontalAlignment.CENTER, TextSymbol.VerticalAlignment.MIDDLE);
-        numText11.setOffsetY(265);
+        numText11.setOffsetY(270);
         numText11.setAngle(330);
 
         numGraphic = new Graphic(centerPoint, numText);
